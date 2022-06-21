77308ee96938513768c4298dec5af659c6f581fb
Takashi <renanyagi@hotmail.com>
2022-06-21 19:43:44 -0400

    adicionando diretorio do exercicio 4.1

---
+++ fundamentos/bloco-4-javascript/scripts.js
@@ -0,0 +1,71 @@
+/* ex2
+let patientId = '50';
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId)
+*/
+/* ex3
+const base = 5;
+let height = 8;
+const area = base * height;
+const perimeter = (2*(base+height));
+
+console.log(area);
+console.log(perimeter);
+*/
+
+/* ex4
+const nota = 80;
+if (nota >= 80) {
+    console.log("Parabéns, você foi aprovada(o)");
+}
+    else if (nota >= 60 && nota < 80) {
+        console.log('Você está na nossa lista de espera');
+    }
+    else {
+        console.log('Você foi reprovada(o)');
+    }
+*/
+/* ex Switch e Case
+let trafficLight = 'amarelo';
+switch (trafficLight) {
+    case "vermelho":
+        console.log('Pare');
+        break;
+
+    case 'amarelo':
+        console.log('Atenção')
+        break;
+
+    case 'verde':
+        console.log('Siga')
+        break;
+
+    default:
+        console.log('Cuidado')
+}
+*/
+/*
+let nota = 'aprovada';
+switch(nota) {
+    case 'aprovada':
+        console.log('Parabens voce foi aprovad');
+        break;
+
+    case 'lista':
+        console.log('Parabens voce está na lista de espera');
+        break;
+
+    case 'reprovada':
+        console.log('Continue tentando');
+        break;
+
+    default:
+        console.log('Procure a administração')
+}
+*/
