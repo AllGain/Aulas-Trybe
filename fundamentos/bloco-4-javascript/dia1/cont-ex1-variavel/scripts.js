e35b581737b7a7aec98a2509db4469a8c5bce972
Takashi <renanyagi@hotmail.com>
2022-06-21 15:24:50 -0400

    adicionando ultimo exercicio 3 e começando bloco 4 Js

---
+++ fundamentos/bloco-4-javascript/dia1/cont-ex1-variavel/scripts.js
@@ -0,0 +1,9 @@
+const myName = "Yagi";
+const birthCity = "Londrina";
+var birthYear = "1992";
+birthYear = 2030;
+birthCity = "Cuiabá";
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
