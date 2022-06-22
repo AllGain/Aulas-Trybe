508b832371f74d179c691db96bdab73bb5616709
Takashi <renanyagi@hotmail.com>
2022-06-21 21:46:30 -0400

    add ex 7

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -4,6 +4,7 @@ const c = 178;
 const abc = a+b+c;
 let peca = 'Torre';
 let pecal = peca.toLowerCase()
+let nota = 90;
 
 const ad = (a+b);
 const sub = (a-b);
@@ -11,6 +12,32 @@ const mult = (a*b);
 const div = (a/b);
 const mod = (a%b);
 
+//ex7
+if (nota>=90 && nota<=100){
+    console.log('Nota A');
+}
+else if (nota>=80 && nota<90) {
+    console.log('Nota B');
+}
+else if (nota>=70 && nota<80) {
+    console.log('Nota C');
+}
+else if (nota>=60 && nota<70) {
+    console.log('Nota D');
+}
+else if (nota>=50 && nota<60) {
+    console.log('Nota E');
+}
+else if (nota<50 && nota>=0) {
+    console.log('Nota F');
+}
+else {
+    console.log('Error, nota invalida')
+}
+
+
+
+/* ex6
 switch (pecal) {
     case 'peão'||'peao' :
         console.log("Peão => Anda 1Casa Frente/Ataca 1Casa diagonal");
