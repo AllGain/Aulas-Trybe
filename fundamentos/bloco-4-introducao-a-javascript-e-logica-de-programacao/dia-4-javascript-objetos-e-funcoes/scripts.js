1ccad5b69b1fbc3edb16b534f522aa03fba37aa5
Takashi <renanyagi@hotmail.com>
2022-06-24 21:17:40 -0400

    adicionando exercicios até o 4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -0,0 +1,36 @@
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  recorrente: 'Sim'
+};
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
+};
+
+//ex2 simples assim? massa!!
+//info.recorrente = 'Sim';
+//console.log(info);
+
+
+//ex5
+
+console.log(info);
+
+/*ex4 passo de imprimir, variavel vai varrer objeto 'info'
+for (let list in info){
+  console.log(info[list]);
+}
+
+/*ex3 passo de imprimir, variavel vai varrer objeto 'info'
+for (let list in info){
+  console.log(list);
+}
+
+/*ex1
+  console.log('Bem-vinda ' + info.personagem);
+*/
