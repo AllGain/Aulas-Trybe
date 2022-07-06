2ee86df8a3137678d8b84e31eab6350399018042
Takashi <renanyagi@hotmail.com>
2022-07-06 19:06:10 -0400

    adicionando exercicio para começar bloco 6

---
+++ fundamentos/bloco-5-js-dom-eventos-e-webstorage/exercicios5.1/exercises/script.js
@@ -0,0 +1,18 @@
+document.querySelector("h1").style.backgroundColor = "green";
+document.querySelector("h1").style.padding = "10px";
+document.querySelectorAll(".emergency-tasks div")[0].style.backgroundColor =
+  "pink";
+document.querySelectorAll(".emergency-tasks div")[0].style.padding = "5px";
+document.querySelectorAll(".emergency-tasks div")[1].style.backgroundColor =
+  "pink";
+document.querySelectorAll(".emergency-tasks div")[1].style.padding = "5px";
+document.querySelectorAll("h3")[0].style.backgroundColor = "purple";
+document.querySelectorAll("h3")[1].style.backgroundColor = "purple";
+document.querySelectorAll(".no-emergency-tasks div")[0].style.backgroundColor =
+  "yellow";
+document.querySelectorAll(".no-emergency-tasks div")[0].style.padding = "5px";
+document.querySelectorAll(".no-emergency-tasks div")[1].style.backgroundColor =
+  "yellow";
+document.querySelectorAll(".no-emergency-tasks div")[1].style.padding = "5px";
+document.querySelectorAll("h3")[2].style.backgroundColor = "black";
+document.querySelectorAll("h3")[3].style.backgroundColor = "black";
